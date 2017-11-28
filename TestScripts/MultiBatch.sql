dd6ee1897505fa9525c417ce0f3cbd53b5c98579
Drew Furgiuele <drewfurgiuele@gmail.com>
2017-11-28 15:51:55 -0500

    Adding a comment and intentional error

--- TestScripts/MultiBatch.sql
+++ TestScripts/MultiBatch.sql
@@ -1,6 +1,7 @@
 ALTER TABLE Person.Address ADD UpdatedFlag BIT
 GO
 
+--Yes, this has an error on purpose
 CREATE NONCLUSTERED INDEX IX_Person_Adddress_UpdatedFlag (UpdatedFlag, StateProvinceID)
 GO
 
